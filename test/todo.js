d1153f6ce257ba51f83f18720b5f3cc744815439
collinsnji <collins.geek@gmail.com>
2018-07-21 23:44:20 -0400

    Add tests

---
+++ test/todo.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const [should, expect] = [chai.should(), chai.expect];
+const { Todo } = require('../build/src/todos');
+
+let t = 'null'
+describe('Running TODO tests..', () => {
+    const todo = new Todo();
+
+    it('todo should be an instance of imported Todo class', () => {
+        expect((typeof todo)).to.equal('object');
+        expect(todo instanceof Todo).to.equal(true);
+    });
+
+    it('Should create a new Todo Item', () => {
+        expect(
+            todo.newTodo({
+                title: 'Test Todo item',
+                body: 'Body of test todo',
+                uuid: '005'
+            })
+        ).to.not.throw;
+    });
+    it('Should get all todo items as an array', () => {
+        expect(typeof todo.showAll()).to.equal('object');
+    });
+})
